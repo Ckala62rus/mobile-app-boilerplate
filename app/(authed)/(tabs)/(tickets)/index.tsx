a0fc8136d77444e1ae1a46b2aecd6f2a24a75dfd
Ckala62rus <agr.akyla@mail.ru>
2025-02-01 19:18:46 +0300

    add video player

--- app/(authed)/(tabs)/(tickets)/index.tsx
+++ app/(authed)/(tabs)/(tickets)/index.tsx
@@ -10,6 +10,7 @@ import { Api } from "@/services/api";
 const videoSource = "http://192.168.1.104:88/video/630be803-1fe1-483e-90ca-2ffad8a3cfa3.mp4";
 
 export default function TicketScreen() {
+  const [volume, setVolume] = useState(1);
 // http://localhost:8001/api/v1/minio/file?file=15ddf394-696f-483b-975c-642adf731da1.jpg&bucket=images
     // const [video, setVideo] = useState<string>("http://localhost:88/video/74cf3789-fa47-4a40-9100-a0dc04694f3b.mp4")
 
@@ -34,12 +35,30 @@ export default function TicketScreen() {
     
     const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
 
+    const increaseVolume = () => {
+      setVolume((oldVolume) => Math.min(oldVolume + 0.1, 1));
+      player.volume = volume
+    };
+  
+    const decreaseVolume = () => {
+      setVolume((oldVolume) => Math.max(oldVolume - 0.1, 0));
+      player.volume = volume
+    };
+
     // return <></>
     return (
         <View style={styles.contentContainer}>
-          <VideoView style={styles.video} player={player} allowsFullscreen allowsPictureInPicture />
+          <VideoView
+            style={styles.video} 
+            player={player} 
+            allowsFullscreen 
+            allowsPictureInPicture
+            
+          />
           <View style={styles.controlsContainer}>
-            <Button
+            <Button title="Increase" onPress={increaseVolume} />
+            <Button title="Decrease" onPress={decreaseVolume} />
+            <Button 
               title={isPlaying ? 'Pause' : 'Play'}
               onPress={() => {
                 if (isPlaying) {
